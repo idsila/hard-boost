b1102ed878b3ab3fd9457fa69811ca09d652278e
idsila <idsila88@gmail.com>
2025-09-04 17:48:15 +0400

    pc_update_04

--- get.js
+++ get.js
@@ -1,13 +1,49 @@
 const axios = require('axios');
-const id = 7502494374;
+const dataBase = require("./dataBase.js");
+const orderBase = require("./orderBase.js");
 
-axios.post('http://localhost:3000/send-ref', { id: id })
-.then(response => {
-  console.log('✅ Сервер ответил:', response.data);
+// const id = 7502494374;
+// const id2 = 1383421742;
+
+// const users = [7502400374, id, 7502400374, 7502400374, 7502400374, id2, 7502400374]
+
+dataBase.find({}).then(users => {
+  users.forEach(user => {
+    if(user.id){
+      axios.post('https://hard-boost-bot.onrender.com/send-ref', { id: user.id })
+      .then(response => {
+        console.log('✅ Сервер ответил:', response.data);
+      })
+      .catch(error => {
+        console.error('❌ Ошибка при отправке запроса:', error.message);
+      });
+    }
+    else{
+      console.log(user)
+    }
+  })
 })
-.catch(error => {
-  console.error('❌ Ошибка при отправке запроса:', error.message);
-});
+
+
+
+// users.forEach(user => {
+//   axios.post('https://hard-boost-bot.onrender.com/send-ref', { id: id })
+//   .then(response => {
+//     console.log('✅ Сервер ответил:', response.data);
+//   })
+//   .catch(error => {
+//     console.error('❌ Ошибка при отправке запроса:', error.message);
+//   });
+// })
+
+
+// axios.post('http://localhost:3000/send-ref', { id: id })
+// .then(response => {
+//   console.log('✅ Сервер ответил:', response.data);
+// })
+// .catch(error => {
+//   console.error('❌ Ошибка при отправке запроса:', error.message);
+// });
 
 
 
